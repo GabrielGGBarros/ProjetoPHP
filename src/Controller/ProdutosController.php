f7f46a10a1487da501b25359aa1dfd6d0bd452d2
GabrielGGBarros <106283241+GabrielGGBarros@users.noreply.github.com>
2022-06-01 20:43:40 -0300

    Create ProdutosController.php

---
+++ src/Controller/ProdutosController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Aluno\ProjetoPHP\Controller;
+
+use Aluno\ProjetoPHP\Model\DAO\ProdutosDAO;
+use Aluno\ProjetoPHP\Model\Entity\Produtos;
+
+class ProdutosController{
+
+    //private ProdutosDAO $dao
+
+    public static function abrirFormularioInserir(){
+        require_once "../src/View/inserir_produto.php";
+    }
+
+    public static function abrirFormularioAlterar($params){
+        $dao = new ProdutosDAO();
+        $resultado = $dao->consultarPorId($params[1]);
+        require_once "../src/View/alterar_produto.php";
+    }
+
+    public static function abrirListaProdutos(){
+        $dao = new ProdutosDAO();
+        $resultado = $dao->consultar();
+        require_once "../src/View/listar_produtos.php";
+    }
+
+    public static function inserirCliente(){
+        $produto = new Produtos();
+        $produto->setId($_POST['id']);
+        $produto->setDescricao=($_POST['descricao']);
+        $produto->setValor($_POST['valor']);
+        $produto->setNome($_POST['nome']);
+        $dao = new ProdutosDAO();
+        if ($dao->inserir($produto)){
+            $resposta = true;
+        } else {
+            $resposta = false;
+        }
+        ClientesController::abrirListaClientes();
+    }
+
+    public static function editarCliente($params){
+        $cliente = new Produtos();
+        $cliente->setEmail($_POST['email']);
+        $cliente->setIdade($_POST['idade']);
+        $cliente->setNome($_POST['nome']);
+        $cliente->setId($params[1]);
+        $dao = new ProdutosDAO();
+        if ($dao->alterar($cliente)){
+            $resposta = true;
+        }else{
+            $resposta = false;
+        }
+        ClientesController::abrirListaProdutos();
+    }
+
+    public static function excluirProduto($params){
+        $dao = new ProdutosDAO();
+        if ($dao->excluir($params[1])){
+            $resposta = true;
+        }else{
+            $resposta = false;
+        }
+        ClientesController::abrirListaProdutos();
+    }
+
+}
